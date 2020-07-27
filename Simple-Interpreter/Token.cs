074a839954265a0a795c10ae2c861beebc0d31d4
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-07-27 20:28:44 +0700

    Add Token class

---
+++ Simple-Interpreter/Token.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simple_Interpreter
+{
+    class Token
+    {
+        private TokenType type;
+        private object value;
+
+        public Token(TokenType type, object value)
+        {
+            this.type = type;
+            this.value = value;
+        }
+
+        public object Value { get => value; set => this.value = value; }
+        internal TokenType Type { get => type; set => type = value; }
+
+        public override string ToString()
+        {
+            return $"Token({type}, {value})";
+        }
+    }
+}
