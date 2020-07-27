764fec9f91578a98c7ebd4927bfd81d37679bc75
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-07-27 20:28:35 +0700

    Add TokenType

---
+++ Simple-Interpreter/TokenType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simple_Interpreter
+{
+    enum TokenType
+    {
+        Int,
+        Plus,
+        EOF
+    }
+}
