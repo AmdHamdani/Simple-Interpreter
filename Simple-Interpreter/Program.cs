1b7cc7fc00ca0e296f2c294d46b9ded1f29ec011
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-07-27 20:29:08 +0700

    Add Interpreter runner

---
+++ Simple-Interpreter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Simple_Interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.Write("Calc > ");
+                string text = Console.ReadLine();
+                Interpreter interpreter = new Interpreter(text);
+                int result = interpreter.Expr();
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
